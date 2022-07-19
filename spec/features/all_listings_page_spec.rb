9ac20f62d3a3e0e333f4208387e80c78192c3507
KMTARBUCK <kmtarbuck@gmail.com>
2022-07-19 14:57:39 +0100

    View listings passing test

--- spec/features/all_listings_page_spec.rb
+++ spec/features/all_listings_page_spec.rb
@@ -1,6 +1,8 @@
 feature 'Viewing all listings' do
   scenario 'User visits all_listings page' do
     visit('/all-listings')
-    expect(page).to have_content "Casa Amore, Full of islanders!, 100"
+    expect(page).to have_content "Name: Casa Amore Description: Full of islanders! Price Per Night: 100"
+    expect(page).to have_content "Name: Phantom Manor Description: Full of islanders! Price Per Night: 100"
+    expect(page).to have_content "Name: House of pain Description: Full of islanders! Price Per Night: 100"
   end
 end
