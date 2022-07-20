36e916af695baa273c6dda467bd9a2f898671f67
Marlon-Buckley <m4rlz@live.co.uk>
2022-07-20 12:12:19 +0100

    adding changes to folder structure, all tests are passing/rackup working

---
+++ spec/features/registration/user_can_sign_up_and_in_spec.rb
@@ -0,0 +1,12 @@
+# require 'spec_helper'
+
+# feature 'Registrations' do
+#   scenario 'user can sign up and be signed in' do
+#     visit('registrations/new')
+#     fill_in('email', with: 'test@test.com')
+#     fill_in('password', with: 'test12')
+#     click_button('submit')
+#     expect(page.current_path).to eq('/')
+#     expect(page).to have_content('Hi, test@test.com')
+#   end
+# end
