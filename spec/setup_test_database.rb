192a18e4d3bdbaf5cf35836eab98bc7bc4df4a5a
Marlon-Buckley <m4rlz@live.co.uk>
2022-07-21 12:31:57 +0100

    Adding changes to database structure and add.erb view

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -4,7 +4,7 @@ p "Setting up test database..."
 
 def setup_test_database
   connection = PG.connect(dbname: 'makersbnb_test')
-  connection.exec("TRUNCATE listings;")
+  connection.exec("TRUNCATE listings RESTART IDENTITY;")
   connection.exec("INSERT INTO listings (name, description, price, available_from, available_to) VALUES ('Casa Amore', 'Full of islanders!', '100', '01/07/22', '01/08/22');")
   connection.exec("INSERT INTO listings (name, description, price, available_from, available_to) VALUES ('Phantom Manor', 'Full of islanders!', '100', '01/12/23', '01/02/23');")
   connection.exec("INSERT INTO listings (name, description, price, available_from, available_to) VALUES ('House of pain', 'Full of islanders!', '100', '01/01/22', '31/12/23');")
